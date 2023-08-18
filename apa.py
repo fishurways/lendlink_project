17052a1eacf2e662481794950180e8ef6083ea6e
fishurways <uways637@gmail,com>
2023-08-18 09:17:17 +0700

    nambahin hello world dan testingrandom

--- apa.py
+++ apa.py
@@ -1,2 +1,20 @@
-print("haloooo")
-print("hello world!")
+from flask import Flask
+app = Flask(__name__)
+#############################################
+import random
+
+@app.route("/")
+def helloworld():
+    return "Hello World!"
+@app.route("/testingrandom")
+def testingrandom():
+    import random
+    dice = [1, 2, 3, 4, 5, 6]
+    randomchoice = random.choice(dice)
+    randomchoice = str(randomchoice)
+    return randomchoice
+    
+
+#############################################
+if __name__ == "__main__":
+    app.run()
