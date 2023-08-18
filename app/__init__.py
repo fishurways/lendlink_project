22289391d6dbbb348c56f49c07d6472e8fa512fd
kholidfu <sopier@gmail.com>
2023-08-18 10:01:45 +0700

    adding base app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__,
             static_folder="static",
             static_url_path="/static",
             )
+
+Bootstrap(app)
