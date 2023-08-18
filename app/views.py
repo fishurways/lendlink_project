36507672dcb0ede5c676e29669a2b545b5f589b4
kholidfu <sopier@gmail.com>
2023-08-18 10:02:03 +0700

    menata code biar rapi

--- app/views.py
+++ app/views.py
@@ -1,15 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+# di sini tempat import common library
+import random
+
+# di sini tempat import library yang khusus untuk project ini
+from flask import render_template
+from app import app
+
 #############################################
 import random
 
 @app.route("/")
-def helloworld():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 @app.route("/testingrandom")
 def testingrandom():
-    import random
     dice = [1, 2, 3, 4, 5, 6]
     randomchoice = random.choice(dice)
     randomchoice = str(randomchoice)
