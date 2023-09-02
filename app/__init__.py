6f0456a20fb2e99106699cff5e5af53e57f7720a
kholidfu <sopier@gmail.com>
2023-09-02 16:12:55 +0700

    musing with base html

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__,
             static_folder="static",
             static_url_path="/static",
             )
 
-Bootstrap(app)
+bootstrap = Bootstrap5(app)
