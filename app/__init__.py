32064ff9f9b5a10a754646c51a549809e0888486
kholidfu <sopier@gmail.com>
2023-10-02 06:18:45 +0700

    adding email to the schema

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,7 @@ db.init_app(app)
 # Membuat model untuk tabel pengguna
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String)
 
