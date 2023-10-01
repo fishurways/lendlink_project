1628cae1a01dc0d0f7628e7ad44cc63b58922a02
kholidfu <sopier@gmail.com>
2023-10-01 23:32:52 +0700

    adding login logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,35 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 
 app = Flask(__name__,
             static_folder="static",
             static_url_path="/static",
             )
 
+class Base(DeclarativeBase):
+    pass
+
+# initiate db through SQLAlchemy
+app.config["SECRET_KEY"] = "12345678"  # Ganti dengan secret key yang aman
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"  # Nama database SQLite
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# Membuat model untuk tabel pengguna
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String)
+
+
+# Membuat tabel jika belum ada
+with app.app_context():
+    db.create_all()
+
+# memanggil template bootsrap5
 bootstrap = Bootstrap5(app)
--- app/views.py
+++ app/views.py
@@ -1,22 +1,44 @@
-# di sini tempat import common library
+from app import app
+from app import User
+
+# di sini tempat import built-in library
 import random
 
 # di sini tempat import library yang khusus untuk project ini
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
-#############################################
-import random
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
     '''This is for login page.'''
-    return render_template("login.html")
+    username = request.form["username"]
+    password = request.form["password"]
 
+    print(username)
+    print(password)
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password, password):
+        # Login sukses
+        flash("Login sukses!", "success")
+        return redirect(url_for("welcome"))
+    else:
+        # Login gagal
+        flash("Login gagal. Periksa kembali username dan password Anda.", "danger")
+        return redirect(url_for("index"))
+
+@app.route("/welcome")
+def welcome():
+    return "Selamat datang! Anda berhasil login."
+
+
+    
 @app.route("/register")
 def register():
     '''This is for register page.'''
